849ff022d01854d5d295e6b7940f663ad32cc8c7
Michael Male <malmike21@gmail.com>
2021-01-31 00:03:43 +0300

    initialise project

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/sample-app'),
+      dir: require('path').join(__dirname, './coverage/pencil-front-end'),
       subdir: '.',
       reporters: [
         { type: 'html' },
