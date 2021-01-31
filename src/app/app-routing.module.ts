14e7ec43d246a6d40382f053ed378395a2b8e592
Michael Male <malmike21@gmail.com>
2021-01-31 13:40:17 +0300

    add default route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { canActivate, redirectUnauthorizedTo, redirectLoggedInTo } from "@angular/fire/auth-guard";
 import { LoginComponent } from "./login/login.component";
-
-const redirectUnAuthorisedToLogin = () => redirectUnauthorizedTo(['login']);
-const redirectLoggedInToTextEditor = () => redirectLoggedInTo(['login']);
 
 const routes: Routes = [
   { path: "", redirectTo: "/login", pathMatch: 'full' },
   {
     path: "login",
     component: LoginComponent,
-    ...canActivate(redirectLoggedInToTextEditor)
   },
+  { path: '**', redirectTo: '', pathMatch: 'full' },
 ];
 
 @NgModule({
