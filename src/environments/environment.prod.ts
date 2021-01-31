4bd54e5a7b9b7600304fcf38a2ae5fe8bbdda68e
Michael Male <malmike21@gmail.com>
2021-02-01 01:44:21 +0300

    add firebase prod setting

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyCcGaOOjZIt-PEu762naZ4bRStzJKxXphk",
+    authDomain: "pencilproject-cda84.firebaseapp.com",
+    projectId: "pencilproject-cda84",
+    storageBucket: "pencilproject-cda84.appspot.com",
+    messagingSenderId: "356788316796",
+    appId: "1:356788316796:web:a35f0813c3dab2eeeb37f9",
+    measurementId: "G-S4WS0SFGKL"
+  }
 };
