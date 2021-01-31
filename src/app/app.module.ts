4e1194788e00a33271f7185214a9917cef577964
Male Michael <malmike21@outlook.com>
2021-02-01 00:37:59 +0300

    Add firebase (#3)
    - add auth service
    - add firebase configs to environment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AngularFireModule } from "@angular/fire";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { environment } from "../environments/environment";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    AngularFireModule.initializeApp(environment.firebaseConfig),
     BrowserModule,
     AppRoutingModule
   ],
