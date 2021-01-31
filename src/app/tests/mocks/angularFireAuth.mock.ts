45a3cfffc295c60e6ac9f39357d598755a23a541
Michael Male <malmike21@gmail.com>
2021-01-31 21:15:57 +0300

    update MockAngularFireAuth

--- src/app/tests/mocks/angularFireAuth.mock.ts
+++ src/app/tests/mocks/angularFireAuth.mock.ts
@@ -4,5 +4,6 @@ import firebase from "firebase/app";
 export default class MockAngularFireAuth {
   user = new ReplaySubject<firebase.User>(1);
   signInWithPopup = () => Promise.resolve();
+  onAuthStateChanged = () => Promise.resolve();
   auth = {};
 }
