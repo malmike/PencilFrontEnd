b787f080260a9354164b2a15e5d4785ecc108608
Michael Male <malmike21@gmail.com>
2021-01-31 08:50:01 +0300

    add route for text editor component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { canActivate, redirectUnauthorizedTo, redirectLoggedInTo } from "@angular/fire/auth-guard";
+
 import { LoginComponent } from "./login/login.component";
+import { TextEditorComponent } from "./text-editor/text-editor.component";
+
+const redirectUnAuthorisedToLogin = () => redirectUnauthorizedTo(['login']);
+const redirectLoggedInToTextEditor = () => redirectLoggedInTo(['text-editor']);
 
 const routes: Routes = [
   { path: "", redirectTo: "/login", pathMatch: 'full' },
   {
     path: "login",
     component: LoginComponent,
+    ...canActivate(redirectLoggedInToTextEditor),
+  },
+  {
+    path: "text-editor",
+    component: TextEditorComponent,
+    ...canActivate(redirectUnAuthorisedToLogin)
   },
   { path: '**', redirectTo: '', pathMatch: 'full' },
 ];
