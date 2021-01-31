5de22b2bc66d963f0e1164b73270c694c4e364fe
Michael Male <malmike21@gmail.com>
2021-01-31 08:45:05 +0300

    add route login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { canActivate, redirectUnauthorizedTo, redirectLoggedInTo } from "@angular/fire/auth-guard";
 import { LoginComponent } from "./login/login.component";
+
+const redirectUnAuthorisedToLogin = () => redirectUnauthorizedTo(['login']);
+const redirectLoggedInToTextEditor = () => redirectLoggedInTo(['login']);
 
 const routes: Routes = [
   { path: "", redirectTo: "/login", pathMatch: 'full' },
